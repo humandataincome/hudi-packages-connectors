64d7c5ae5c7f1884bfb0632c5b066de592e5658e
Antonio Alfonso <antonioalfonso.work@gmail.com>
2022-03-24 19:25:55 +0100

    fix: validator/index.ts

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -1,16 +1,16 @@
 import {DataSourceCode} from "../descriptor";
 import {ValidatorInstagram} from "./validator.instagram";
 import {ValidatorFacebook} from "./validator.facebook";
-import {ValidatorAmazon} from "./validator.amazon";
+// import {ValidatorAmazon} from "./validator.amazon";
 
 export * from './validator';
 export * from './validator.error';
 export * from './validator.instagram';
 export * from './validator.facebook';
-export * from './validator.amazon';
+// export * from './validator.amazon';
 
 export default  {
     [DataSourceCode.INSTAGRAM]: ValidatorInstagram,
     [DataSourceCode.FACEBOOK]: ValidatorFacebook,
-    [DataSourceCode.AMAZON]: ValidatorAmazon,
+    // [DataSourceCode.AMAZON]: ValidatorAmazon,
 };
