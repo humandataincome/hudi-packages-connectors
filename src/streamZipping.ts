eb7cc30f2ac341b0d66c0b6d153c37e3c1cb6b21
Davide Macchia <davide.macchia95@gmail.com>
2022-06-01 18:41:37 +0200

    fix: testing fflate

--- src/streamZipping.ts
+++ src/streamZipping.ts
@@ -1,19 +1,12 @@
-import * as ZipJS from "@zip.js/zip.js";
 
+import {ZipInputFile} from "fflate";
 export class StreamZipping {
 
 
+    static async validateZipStream(zipFile: Buffer): Promise<void> {
+        const massiveFile = new Uint8Array(zipFile);
+        const massiveAgain = ZipInputFile(massiveFile);
 
-    static async validateZipStream(zipFile: Uint8Array): Promise<void> {
-        const ZipJS = require("@zip.js/zip.js");
-       const zipReader = new ZipJS.ZipReader(new ZipJS.Uint8ArrayReader(zipFile));
-       await zipReader.getEntries({
-           onprogress: ((progress: number, total: number, entry: ZipJS.Entry) => {
-               console.log(progress);
-               console.log(total);
-               console.log(entry)
-           })
-       });
     }
 
 }
