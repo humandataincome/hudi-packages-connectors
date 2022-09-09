e3ac8994ae8339a13c625bf02d1080c31e3b4e27
DavideMacchia <davide.macchia95@gmail.com>
2022-09-09 10:26:22 +0200

    feat: netflix processor

---
+++ src/source/netflix/processor.netflix.ts
@@ -0,0 +1,52 @@
+import {staticImplements} from "../../utils/decorator";
+import {ProcessorDatasource, ProcessorOptions, ValidatorObject} from "../../utils";
+import Logger from "../../utils/logger";
+import {NetflixDataAggregator} from "./model.netflix";
+import {Unzipped, unzipSync} from "fflate";
+import {FileCodeNetflix} from "./enum.netflix";
+import {ServiceNetflix} from "./service.netflix";
+
+@staticImplements<ProcessorDatasource>()
+export class ProcessorNetflix {
+    private static readonly logger = new Logger("Processor Netflix");
+
+    static initAggregator(): NetflixDataAggregator {
+        return {};
+    }
+
+    static async zipAggregatorBuilder(zipFile: Uint8Array, options?: ProcessorOptions): Promise<NetflixDataAggregator | undefined> {
+        try {
+            const files: Unzipped = unzipSync(zipFile);
+            const model: NetflixDataAggregator = this.initAggregator();
+            for (let pathName in files) {
+                const file = files[pathName];
+                const data = Buffer.from(file, file.byteOffset, file.length);
+                if (!ValidatorObject.isDirectory(pathName)) {
+                    await this.aggregatorBuilder(data, pathName, model, options);
+                }
+            }
+            if (!ValidatorObject.objectIsEmpty(model)) {
+                model.creationDate = new Date();
+                return model;
+            }
+        } catch (error: any) {
+            (error && error.message) && (this.logger.log('error', error.message, 'zipAggregatorBuilder'));
+            if (options && options.throwExceptions !== undefined && options.throwExceptions) {
+                throw error;
+
+            }
+        }
+        return undefined;
+    }
+
+    static async aggregatorBuilder(data: Buffer, pathName: string, model: NetflixDataAggregator, options: ProcessorOptions = {}) {
+        let result, regex;
+        if ((regex = new RegExp(FileCodeNetflix.)) && (regex.test(pathName))) {
+            result = await ServiceNetflix.parsePersonalInformation(data);
+            (result) && (model.profile.emails = result);
+        } else if ((regex = new RegExp(FileCodeNetflix.PROFILE)) && (regex.test(pathName))) {
+            result = await ServiceLinkedin.parseProfile(data);
+            (result) && (model.profile.info = result);
+        }
+    }
+}
