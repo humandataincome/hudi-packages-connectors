56f9d33404ccd25be8d887ab986e524128419f7b
DavideMacchia <davide.macchia95@gmail.com>
2022-09-16 12:15:10 +0200

    fix: signature compatible with frontend

--- src/source/binance/service.binance.ts
+++ src/source/binance/service.binance.ts
@@ -1,60 +1,70 @@
-import {AccountBI} from "./model.binance";
+import {APIAccountBI} from "./model.binance";
 import {ValidatorObject} from "../../utils";
 import Logger from "../../utils/logger";
 import axios from "axios";
 
+interface ConfigAPIRequest {
+    url: string;
+    method: string;
+    headers: Record<string,string>;
+}
+
 export class ServiceBinance {
     private static readonly logger = new Logger("Service Binance");
 
-    static isBrowser(){
+    private static isBrowser(){
         return typeof window !== "undefined" && typeof window.document !== "undefined";
     }
-    static createSignature(queryString: string, apiSecret: string) {
-        const crypto = require('crypto');
-        return crypto
-            .createHmac('sha256', apiSecret)
-            .update(queryString)
-            .digest('hex');
+
+    private static createSignature(queryString: string, apiSecret: string) {
+        const CryptoJS = require("crypto-js");
+        return CryptoJS.HmacSHA256(queryString, apiSecret).toString();
     }
 
-    static async getAccount(apiKey: string, apiSecret: string): Promise<AccountBI | undefined> {
+    private static async APIRequest(apiSecret: string, config: ConfigAPIRequest): Promise<any> {
+        const queryString = 'timestamp=' + Date.now();
+        const signatureBinance = this.createSignature(queryString, apiSecret);
+        config = {...config, url: `${config.url}?${queryString}&signature=${signatureBinance}`};
+
+        if (!this.isBrowser()) { //if NOT browser
+            return axios(config);
+        } else { //if browser, then must get around CORS
+            //TODO: use HDN from hudi-protocol-core
+            return
+        }
+    }
+
+    /**
+     * API call to https://api.binance.com/api/v3/account
+     */
+    static async getAccountAPI(apiKey: string, apiSecret: string): Promise<APIAccountBI | undefined> {
         try {
-            let response;
-            const queryString = 'timestamp=' + Date.now();
-            const signatureBinance = this.createSignature(queryString, apiSecret);
             const config = {
-                url: `https://api.binance.com/api/v3/account?${queryString}&signature=${signatureBinance}`,
+                url: `https://api.binance.com/api/v3/account`,
                 method: 'GET',
                 headers: {
                     'X-MBX-APIKEY': apiKey
                 }
             };
-            if (!this.isBrowser()) { //if NOT browser
-                response = await axios(config);
-            } else { //if browser, then must get around CORS
-                //use HDN from sdk
-                //response =
-            }
-            return this.buildAccountBI(response);
+            const response = await this.APIRequest(apiSecret, config);
+
+            const model: APIAccountBI = {};
+            model.makerCommission = response.data.makerCommission;
+            model.takerCommission = response.data.takerCommission;
+            model.buyerCommission = response.data.buyerCommission;
+            model.sellerCommission = response.data.sellerCommission;
+            model.canTrade = response.data.canTrade;
+            model.canWithdraw = response.data.canWithdraw;
+            model.canDeposit = response.data.canDeposit;
+            model.updateTime = response.data.updateTime;
+            model.accountType = response.data.accountType;
+            model.balances = response.data.balances;
+            model.permissions = response.data.permissions;
+            return !ValidatorObject.objectIsEmpty(model) ? model : undefined;
         } catch(error: any) {
             this.logger.log('error', error.message, 'getAccount');
             throw error;
         }
     }
 
-    private static buildAccountBI(response: any): AccountBI | undefined {
-        const model: AccountBI = {};
-        model.makerCommission = response.data.makerCommission;
-        model.takerCommission = response.data.takerCommission;
-        model.buyerCommission = response.data.buyerCommission;
-        model.sellerCommission = response.data.sellerCommission;
-        model.canTrade = response.data.canTrade;
-        model.canWithdraw = response.data.canWithdraw;
-        model.canDeposit = response.data.canDeposit;
-        model.updateTime = response.data.updateTime;
-        model.accountType = response.data.accountType;
-        model.balances = response.data.balances;
-        model.permissions = response.data.permissions;
-        return !ValidatorObject.objectIsEmpty(model) ? model : undefined;
-    }
 }
