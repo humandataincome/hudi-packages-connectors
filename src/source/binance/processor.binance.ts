8ef826a14a747969bfe83c4577bc09acd5d0a8c8
DavideMacchia <davide.macchia95@gmail.com>
2022-09-27 11:16:57 +0200

    fix: binance and tiktok processor, model and comment on descriptor.enum.ts

--- src/source/binance/processor.binance.ts
+++ src/source/binance/processor.binance.ts
@@ -72,5 +72,6 @@ export class ProcessorBinance {
         } catch(error: any) {
             this.logger.log('error', error.message, 'aggregatorBuilder')
         }
+        return undefined;
     }
 }
