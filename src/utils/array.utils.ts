e7aa790c4a2213930c73f387eab0ff8e4831b5b9
DavideMacchia <davide.macchia95@gmail.com>
2024-07-08 16:25:02 +0200

    Fix style code

--- src/utils/array.utils.ts
+++ src/utils/array.utils.ts
@@ -1,4 +1,7 @@
-export function sliceIfTooLong<T>(array: T[], maxEntitiesPerArray: number = Infinity): T[] {
+export function sliceIfTooLong<T>(
+    array: T[],
+    maxEntitiesPerArray: number = Infinity,
+): T[] {
     if (array.length > maxEntitiesPerArray) {
         return array.slice(0, maxEntitiesPerArray);
     } else {
